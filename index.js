f29c9cac87a77bb62109ecc164ac23d78d139357
Michael Sermersheim <m.sermersheim@gmail.com>
2017-11-25 16:46:14 -0800

    variable name added, config added for 5 routes

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-angular.module('courseViewer', []);
+var appModule = angular.module('courseViewer', ['ui.router']);
+
+appModule.config(function ($stateProvider) {
+
+    var states = [
+        {
+            name: 'home',
+            url: '/',
+            template: '<home />'
+        },
+        {
+            name: 'home',
+            url: '',
+            template: '<home />'
+        }
+        {
+            name: 'about',
+            url: '/about',
+            template: '<about />'
+        },
+        {
+            name: 'products',
+            url: '/products',
+            template: '<products-services />'
+        },
+        {
+            name: 'ups',
+            url: '/ups',
+            template: '<ups-consolidation />'
+        },
+        {
+            name: 'contact',
+            url: '/contact',
+            template: '<contact-us />'
+        }
+    ];
+
+    states.forEach(function(state) {
+        $stateProvider.state(state);
+    });
+});
